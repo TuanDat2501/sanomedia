b17ca98e0450869f097bd8fac20811028bb0390c
TuanDat2501 <tuandat2514.work@gmail.com>
2024-05-14 15:07:03 +0700

    fix build

--- src/constant/const.ts
+++ src/constant/const.ts
@@ -19,7 +19,7 @@ export const PRESENT_DATA = [
 ]
 export const NEWS_SANOLIFE_DATA = [
     {
-        image:"/image/sanolife-2.jpg",
+        image:"./image/sanolife-2.jpg",
         title: "Chương trình hoạt động: Gala Dinner",
         date: "05-02-2024",
         subText: "Một đêm tiệc đầy niềm vui và ý nghĩa khi toàn thể nhân sự Công ty cùng gia đình cùng nhau nhìn lại hành trình một năm đầy biến động nhưng cũng không kém phần rực rỡ của Sano Media. Để rồi sau đó những giải thưởng vinh danh các cá nhân, tập thể xuất sắc trong năm qua đã được hô vang.",
