5b4a35c959d7f7430ecd5597b04bbcc1c9bb12c6
TuanDat2501 <tuandat2514.work@gmail.com>
2024-05-18 11:22:27 +0700

    fix load image

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
     output: 'export',
     distDir: 'dist',
     images:{
-        unoptimized:false,
+        unoptimized:true,
+        disableStaticImages:true,
     },
     experimental: {
         missingSuspenseWithCSRBailout: false,
